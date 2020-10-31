e215ee869de6089a953e67dfbd298d2a0bd56b19
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-10-31 11:20:07 -0600

    React Apollo prepared

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://rickandmortyapi.com/graphql/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
