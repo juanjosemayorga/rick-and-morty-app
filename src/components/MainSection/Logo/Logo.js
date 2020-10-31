9ce2e1d0f53fa9bef020f781ef9c284812d6336e
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-10-31 13:40:43 -0600

    Logo added

--- src/components/MainSection/Logo/Logo.js
+++ src/components/MainSection/Logo/Logo.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import LOGO from '../../../assets/rick_and_morty.svg'
+import {
+  IMG,
+  FIGURE
+} from './styles'
 
 export const Logo = () => {
   return (
-    <div>
-      <p>Logo</p>
-    </div>
+    <FIGURE>
+      <IMG src={LOGO} alt="Rick and Morty Logo"/>
+    </FIGURE>
   )
 }
---
+++ src/components/MainSection/Logo/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const FIGURE = styled.figure`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 220px;
+`;
+
+export const IMG = styled.img`
+  width: 423px;
+  transition: 0.3s;
+  &:hover {
+    width: 487px;
+  }
+`;
