aba47bf728bc36e0db25d53dac82a40b5e3fb8ca
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-11-01 05:36:08 -0600

    Logo component with SVG finished

--- src/components/MainSection/Logo/styles.js
+++ src/components/MainSection/Logo/styles.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components'
 
-export const FIGURE = styled.figure`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 220px;
-`;
-
-export const IMG = styled.img`
+export const SVG = styled.svg`
   width: 423px;
   transition: 0.3s;
   &:hover {
     width: 487px;
   }
+  &:hover g {
+    fill: #A8DDA8;
+  }
+`;
+
+export const G = styled.g`
+  transition: 0.3s;
 `;
