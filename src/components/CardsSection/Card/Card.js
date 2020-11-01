baf1a124b71c808085fbe1adcafa2ecaa0fbf228
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-11-01 08:55:46 -0600

    Files for the CardsSection and folders created

---
+++ src/components/CardsSection/Card/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div>
+      This is a card
+    </div>
+  )
+}
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,11 +4,13 @@ import {
 } from './styles'
 
 import { MainSection } from '../../components/MainSection/MainSection'
+import { ListCards } from '../../components/CardsSection/ListCards'
 
 export const Home = () => {
   return (
     <Container>
       <MainSection />
+      <ListCards />
     </Container>
   )
 }
